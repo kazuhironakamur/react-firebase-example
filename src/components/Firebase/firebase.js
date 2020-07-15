91ef3f705fb9e81f24c9ba59ad169d72cefedfd3
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2020-07-15 11:39:43 +0900

    ReactとFirebaseを接続

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,20 @@
+import app from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+  }
+}
+
+export default Firebase;
--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,20 +1,6 @@
-import app from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  databaseURL: process.env.REACT_APP_databaseURL,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
-};
-
-class Firebase {
-  constructor() {
-    app.initializeApp(firebaseConfig);
-  }
-}
+import FirebaseContext from "./context";
+import Firebase from "./firebase";
 
 export default Firebase;
+
+export { FirebaseContext };
