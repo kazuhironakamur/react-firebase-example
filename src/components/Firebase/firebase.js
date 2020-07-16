36c5a377d1dce42d1d25f98c09a213b50f7b9adf
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2020-07-16 12:58:52 +0900

    realtime databaseをreactに接続

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -17,6 +17,7 @@ class Firebase {
     app.initializeApp(firebaseConfig);
 
     this.auth = app.auth();
+    this.db = app.database();
   }
 
   // *** Auth API ***
@@ -33,6 +34,11 @@ class Firebase {
 
   doPasswordUpdate = (password) =>
     this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+  user = (uid) => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref("users");
 }
 
 export default Firebase;
